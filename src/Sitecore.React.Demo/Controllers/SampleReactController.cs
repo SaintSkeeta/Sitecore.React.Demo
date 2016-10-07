dee044a65646323e121fccec89bf85bf0c49595b
Sean Holmesby <sean_holmesby@cable.comcast.com>
2016-10-07 14:31:11 -0400

    Added SampleReactRendering to the home item.
     - also added relevant script tags to main layout.

---
+++ src/Sitecore.React.Demo/Controllers/SampleReactController.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.React.Mvc.Controllers;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.React.Demo.Controllers
+{
+    public class SampleReactController : Controller
+    {
+        // GET: SampleReact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SampleReactRendering()
+        {
+            var data = new
+            {
+                Title = FieldRenderer.Render(Sitecore.Context.Item, "Title"),
+                Text = FieldRenderer.Render(Sitecore.Context.Item, "Text")
+            };
+
+            return this.React("~/views/SampleReact/SampleReactRendering.jsx", data);
+        }
+    }
+}
---
+++ src/Sitecore.React.Demo/Views/SampleReact/SampleReactRendering.jsx
@@ -0,0 +1,10 @@
+﻿var SampleReactRendering = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1 dangerouslySetInnerHTML={{__html: this.props.data.Title}}></h1>
+                <div dangerouslySetInnerHTML={{__html: this.props.data.Text}}></div>
+            </div>
+        );
+}
+});
